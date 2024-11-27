94b51634dd24c48c311cfd3a0cf4903e88ebe3dc
Mike <mike.in.flames@gmail.com>
2024-11-27 15:06:28 +0100

    Solve day 1 part 1.

--- src/day01.rs
+++ src/day01.rs
@@ -8,9 +8,42 @@ pub fn get_day() -> Day {
 }
 
 fn solve_part1(input: &str) -> String {
-    println!("{}", input);
+    let directions = vec![
+        vec![0, -1], // North
+        vec![1, 0],  // East
+        vec![0, 1],  // South
+        vec![-1, 0], // West
+    ];
+    let mut current_dir = 0;
+    let mut pos = vec![0i32, 0i32];
 
-    "17".to_string()
+    for cmd in input.split(", ") {
+        let (turn, steps_as_string) = cmd.split_at(1);
+        let steps: i32 = steps_as_string
+            .parse()
+            .expect("Error parsing integer (steps):");
+
+        if turn == "R" {
+            if current_dir == 3 {
+                current_dir = 0;
+            } else {
+                current_dir += 1;
+            }
+        } else {
+            if current_dir == 0 {
+                current_dir = 3;
+            } else {
+                current_dir -= 1;
+            }
+        }
+
+        pos[0] += directions[current_dir][0] * steps;
+        pos[1] += directions[current_dir][1] * steps;
+    }
+
+    let distance = pos[0].abs() + pos[1].abs();
+
+    distance.to_string()
 }
 
 fn solve_part2(input: &str) -> String {
@@ -25,12 +58,14 @@ mod tests {
 
     #[test]
     fn test_part1_with_examples() {
-        assert_eq!(1 + 1, 2);
+        assert_eq!(solve_part1("R2, L3"), "5");
+        assert_eq!(solve_part1("R2, R2, R2"), "2");
+        assert_eq!(solve_part1("R5, L5, R5, R3"), "12");
     }
 
     #[test]
     fn test_part1_with_input() {
-        assert_eq!(solve_part1(&get_day().read_input()), "17");
+        assert_eq!(solve_part1(&get_day().read_input()), "288");
     }
 
     #[test]
