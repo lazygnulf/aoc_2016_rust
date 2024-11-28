8940edb08da4cb32991b1ad48e4073a49a3a2a56
Mike <mike.in.flames@gmail.com>
2024-11-28 17:20:05 +0100

    Solve day 1 part 2.

--- src/day01.rs
+++ src/day01.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use crate::util::Day;
 
 const DAY_NR: u8 = 1;
@@ -8,6 +10,16 @@ pub fn get_day() -> Day {
 }
 
 fn solve_part1(input: &str) -> String {
+    follow_directions(input, false)
+}
+
+fn solve_part2(input: &str) -> String {
+    follow_directions(input, true)
+}
+
+fn follow_directions(input: &str, is_part_2: bool) -> String {
+    let mut visited = HashSet::new();
+
     let directions = vec![
         vec![0, -1], // North
         vec![1, 0],  // East
@@ -17,7 +29,11 @@ fn solve_part1(input: &str) -> String {
     let mut current_dir = 0; // index in directions vector
     let mut pos = vec![0i32, 0i32];
 
-    for cmd in input.split(", ") {
+    if is_part_2 {
+        visited.insert(pos.clone());
+    }
+
+    'cmd_loop: for cmd in input.split(", ") {
         let (turn, steps_as_string) = cmd.split_at(1);
         let steps: i32 = steps_as_string
             .parse()
@@ -29,19 +45,26 @@ fn solve_part1(input: &str) -> String {
             _ => panic!("Invalid direction"),
         }
 
-        pos[0] += directions[current_dir][0] * steps;
-        pos[1] += directions[current_dir][1] * steps;
+        if is_part_2 {
+            for _ in 0..steps {
+                pos[0] += directions[current_dir][0];
+                pos[1] += directions[current_dir][1];
+
+                if visited.contains(&pos) {
+                    break 'cmd_loop;
+                } else {
+                    visited.insert(pos.clone());
+                }
+            }
+        } else {
+            pos[0] += directions[current_dir][0] * steps;
+            pos[1] += directions[current_dir][1] * steps;
+        }
     }
 
     let distance = pos[0].abs() + pos[1].abs();
 
     distance.to_string()
-}
-
-fn solve_part2(input: &str) -> String {
-    println!("{}", input);
-
-    "42".to_string()
 }
 
 #[cfg(test)]
@@ -62,11 +85,11 @@ mod tests {
 
     #[test]
     fn test_part2_with_examples() {
-        assert_eq!(1 + 1, 2);
+        assert_eq!(solve_part2("R8, R4, R4, R8"), "4");
     }
 
     #[test]
     fn test_part2_with_input() {
-        assert_eq!(solve_part2(&get_day().read_input()), "42");
+        //assert_eq!(solve_part2(&get_day().read_input()), "42");
     }
 }
